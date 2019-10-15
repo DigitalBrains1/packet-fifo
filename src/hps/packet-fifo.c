4cfa87268e043c440233a28963825d6d3a17e424
Peter Lebbing <peter@digitalbrains.com>
2019-10-15 16:57:37 +0200

    Initial commit
    Just sysid and Knight Rider LEDs.

---
+++ src/hps/packet-fifo.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "hwlib.h"
+#include "socal/socal.h"
+#include "socal/hps.h"
+#include "socal/alt_gpio.h"
+#include "hps_0.h"
+
+#define HW_REGS_BASE ( ALT_STM_OFST )
+#define HW_REGS_SPAN ( 0x04000000 )
+#define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
+
+int main() {
+
+	uint8_t *virtual_base;
+	int fd;
+	int loop_count;
+	int led_direction;
+	int led_mask;
+	volatile uint32_t *h2p_lw_led_addr;
+	volatile uint32_t *h2p_sysid_addr;
+	uint32_t id;
+
+	if ((fd=open("/dev/mem", (O_RDWR | O_SYNC))) == -1) {
+		perror("Could not open /dev/mem");
+		return 1;
+	}
+
+	virtual_base = mmap(NULL, HW_REGS_SPAN, (PROT_READ | PROT_WRITE),
+			MAP_SHARED, fd, HW_REGS_BASE);
+
+	if (virtual_base == MAP_FAILED) {
+		perror("mmap() failed");
+		close(fd);
+		return 1;
+	}
+
+
+	h2p_lw_led_addr = (uint32_t *) (virtual_base +
+		((size_t) (ALT_LWFPGASLVS_OFST + LED_PIO_BASE) &
+		 (size_t) HW_REGS_MASK));
+	h2p_sysid_addr = (uint32_t *) (virtual_base +
+		((size_t) (ALT_LWFPGASLVS_OFST + SYSID_QSYS_BASE) &
+		 (size_t) HW_REGS_MASK));
+
+	id = *h2p_sysid_addr;
+	printf("%#010x\n", id);
+
+	// toggle the LEDs a bit
+
+	loop_count = 0;
+	led_mask = 0x01;
+	led_direction = 0; // 0: left to right direction
+	while (loop_count < 60) {
+		// control led
+		*h2p_lw_led_addr = led_mask;
+		// wait 100ms
+		usleep (100*1000);
+		// update led mask
+		if (led_direction == 0) {
+			led_mask <<= 1;
+			if (led_mask == (0x01 << (LED_PIO_DATA_WIDTH-1)))
+				led_direction = 1;
+		}else{
+			led_mask >>= 1;
+			if (led_mask == 0x01){
+				led_direction = 0;
+				loop_count++;
+			}
+		}
+	}
+
+	// clean up our memory mapping and exit
+	if (munmap(virtual_base, HW_REGS_SPAN) != 0 ) {
+		perror("munmap() failed");
+		close(fd);
+		return 1;
+	}
+	close(fd);
+	return 0;
+}
---
+++ src/hps/reg_defs.h
@@ -0,0 +1,3 @@
+#define H2F_BASE ( 0xc0000000 )
+#define H2F_SPAN ( 0x3c000000 )
+#define H2F_MASK ( H2F_SPAN - 1 )
