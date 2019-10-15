2f8b39fc5c95a042b9230048c6352c6d8949c8bd
Peter Lebbing <peter@digitalbrains.com>
2019-10-15 18:01:47 +0200

    Code cleanup

--- src/hps/packet-fifo.c
+++ src/hps/packet-fifo.c
@@ -12,9 +12,19 @@
 #define HW_REGS_SPAN ( 0x04000000 )
 #define HW_REGS_MASK ( HW_REGS_SPAN - 1 )
 
-int main() {
+static inline void
+write_reg32(volatile void *base, size_t offset, uint32_t data) {
+	*(volatile uint32_t *) (((volatile char *) base) + offset) = data;
+}
 
-	uint8_t *virtual_base;
+static inline uint32_t
+read_reg32(volatile void *base, size_t offset) {
+	return *(volatile uint32_t *) (((volatile char *) base) + offset);
+}
+
+int
+main() {
+	void *virtual_base;
 	int fd;
 	int loop_count;
 	int led_direction;
@@ -37,15 +47,14 @@ int main() {
 		return 1;
 	}
 
+	h2p_lw_led_addr = (uint32_t *) (((char *) virtual_base) +
+			((size_t) (ALT_LWFPGASLVS_OFST + LED_PIO_BASE) &
+			 (size_t) HW_REGS_MASK));
+	h2p_sysid_addr = (uint32_t *) (((char *) virtual_base) +
+			((size_t) (ALT_LWFPGASLVS_OFST + SYSID_QSYS_BASE) &
+			 (size_t) HW_REGS_MASK));
 
-	h2p_lw_led_addr = (uint32_t *) (virtual_base +
-		((size_t) (ALT_LWFPGASLVS_OFST + LED_PIO_BASE) &
-		 (size_t) HW_REGS_MASK));
-	h2p_sysid_addr = (uint32_t *) (virtual_base +
-		((size_t) (ALT_LWFPGASLVS_OFST + SYSID_QSYS_BASE) &
-		 (size_t) HW_REGS_MASK));
-
-	id = *h2p_sysid_addr;
+	id = read_reg32(h2p_sysid_addr, 0);
 	printf("%#010x\n", id);
 
 	// toggle the LEDs a bit
@@ -55,7 +64,7 @@ int main() {
 	led_direction = 0; // 0: left to right direction
 	while (loop_count < 60) {
 		// control led
-		*h2p_lw_led_addr = led_mask;
+		write_reg32(h2p_lw_led_addr, 0, led_mask);
 		// wait 100ms
 		usleep (100*1000);
 		// update led mask
